5aa3c4c05dd17f3a38245c94ddd2e93d8c9ab5b1
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-03 12:12:18 +0530

    First
    This is my first Java program.

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+        System.out.println("How are you?");
+
+        System.out.println("This is my first program in JAVA");
+        System.out.println("Learning about different concepts in JAVA...");
+    }
+}

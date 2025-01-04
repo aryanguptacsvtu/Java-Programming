b337e6c1e98efd724fa6131654573ea1ddd6929e
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-04 17:12:33 +0530

    Variables2

---
+++ Variables2.java
@@ -0,0 +1,13 @@
+public class Variables2 {
+
+    public static void main(String args[]) {
+
+        int num1 = 12;
+        int num2 = 20;
+        int num3 = 10;
+        int sum = num1 + num2 + num3;
+
+        System.out.print("The sum of these numbers is:");
+        System.out.println(sum);
+    }
+}

5068b5469bec2ccacf3b451f46d745e24be5483d
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-04 17:12:07 +0530

    Variables
    Learning about different kinds of Variables in JAVA...

---
+++ Variables1.java
@@ -0,0 +1,25 @@
+public class Variables1 {
+    public static void main(String[] args) {
+        int age1 = 23;
+        short age2 = 44;
+        byte age3 = 67;
+        long age4 = 566668289572L;
+
+        char ch = 'A';
+        String str = "Hello World!";
+
+        float f1 = 5.67f;
+        double d1 = 4.75D;
+        double d2 = 7.55;
+
+        boolean b = true;
+
+        System.out.println("Integral Values : " + age1 + ", " + age2 + ", " + age3 + ", " + age4);
+
+        System.out.println("String : " + str);
+        System.out.println("Character : " + ch);
+        System.out.println("Boolean : " + b);
+
+        System.out.println("Decimal Values : " + f1 + ", " + d1 + ", " + d2);
+    }
+}

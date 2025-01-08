7f466fb7a107c8e35ff1aac5837f2d2648a2270b
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-08 17:00:37 +0530

    Loops2
    Learning about "for" loop in Java...

---
+++ Loops2.java
@@ -0,0 +1,16 @@
+public class Loops2 {
+    public static void main(String[] args) {
+
+        int n = 10;
+        System.out.println("Using FOR loop:-");
+        System.out.println("Incrementing For loop:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(2 * i + 1);
+        }
+
+        System.out.println("\nDecrementing For loop:");
+        for (int i = 8; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
